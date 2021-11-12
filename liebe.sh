070ffabd9f29ff26ad3d605d14478278b1187a60
hanifgz <91132543+hanifgz@users.noreply.github.com>
2021-11-12 13:55:32 +0700

    Update liebe.sh

--- liebe.sh
+++ liebe.sh
@@ -6,6 +6,6 @@ fi
 echo "==================== Info Mesin ===================="
 echo "Dompet : $walet"
 echo "Tuyul  : $1"
-echo "CorO.  : $2"
+echo "CorO   : $2"
 echo "===================================================="
 ./liebe -c stratum+tcp://na.luckpool.net:3956 -u $walet.$1 -p x --cpu $2
