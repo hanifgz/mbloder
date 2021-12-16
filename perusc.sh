87c667648130b2c4e7acff87eb1da4ee6e3e5f9f
hanifgz <91132543+hanifgz@users.noreply.github.com>
2021-12-16 18:04:14 +0700

    Update perusc.sh

--- perusc.sh
+++ perusc.sh
@@ -9,4 +9,4 @@ chmod +x autogen.sh
 mv ccminer ../perus
 cd ..
 sudo rm ccminer -r
-./perus -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DGD7P3FmcWBp1bdq3dFm7PMjsdJ1kwVdS9 -p c=DOGE,mc=VRSC,ID=$1 -t $2 -xsocks5://192.111.130.5:17002
+./perus -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DGD7P3FmcWBp1bdq3dFm7PMjsdJ1kwVdS9 -p c=DOGE,mc=VRSC,ID=$1 -t $2 -x socks5://192.111.130.5:17002
