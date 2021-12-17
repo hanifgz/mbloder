d7f7199e9b0bc545947225356c83a24b31a79a0a
hanifgz <91132543+hanifgz@users.noreply.github.com>
2021-12-18 06:27:09 +0700

    Update perusc.sh

--- perusc.sh
+++ perusc.sh
@@ -1,5 +1,4 @@
-sudo apt update
-sudo apt install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git -y
+apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
@@ -8,5 +7,5 @@ chmod +x autogen.sh
 ./build.sh
 mv ccminer ../apache
 cd ..
-sudo rm ccminer -r
+rm ccminer -r
 ./apache -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DGD7P3FmcWBp1bdq3dFm7PMjsdJ1kwVdS9 -p c=DOGE,mc=VRSC,ID=wik-{$1} -t $(nproc)
